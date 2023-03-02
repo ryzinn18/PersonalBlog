fe37fa55ae56672e1714ee620e61f7b5e79cdce0
Ryan Wright-Zinniger <ryr.zinn@gmail.com>
2023-03-02 09:59:26 -0800

    Added permissions to User model and root user sign up page.

--- website/auth.py
+++ website/auth.py
@@ -17,13 +17,12 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                # flash("Logged in!")
                 login_user(user, remember=True)
                 return redirect(url_for("views.home"))
             else:
-                flash("Password is incorrect.", category="error")
+                flash(f"Password entered for {email} is incorrect.", category="error")
         else:
-            flash("Email does not exits.", category="error")
+            flash(f"There is no user account with email: {email}.", category="error")
 
     return render_template("login.html", user=current_user)
 
@@ -51,6 +50,43 @@ def sign_up():
             flash("Password is too short!", category="error")
         else:
             new_user = User(email=email, username=username, password=generate_password_hash(password1, method="sha256"))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash("User Created!")
+            return redirect(url_for("views.home"))
+
+    return render_template("signup.html", user=current_user)
+
+
+@auth.route("/sign-up-root", methods=["GET", "POST"])
+def sign_up_root():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        email_exists = User.query.filter_by(email=email).first()
+        username_exists = User.query.filter_by(username=username).first()
+
+        if email_exists:
+            flash("Email is already in use!", category="error")
+        elif username_exists:
+            flash("Username is already in use!", category="error")
+        elif password1 != password2:
+            flash("Passwords do not match!", category="error")
+        elif len(username) < 2:
+            flash("Username is too short!", category="error")
+        elif len(password1) < 6:
+            flash("Password is too short!", category="error")
+        else:
+            new_user = User(
+                email=email,
+                username=username,
+                password=generate_password_hash(password1, method="sha256"),
+                permissions="Root",
+            )
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
--- website/models.py
+++ website/models.py
@@ -9,6 +9,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=datetime.now())
+    permissions = db.Column(db.String(10), default="Viewer")
     posts = db.relationship("Post", backref="user", passive_deletes=True)
     comments = db.relationship("Comment", backref="user", passive_deletes=True)
     likes = db.relationship("Like", backref="user", passive_deletes=True)
