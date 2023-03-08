7bbb8c17434cb0987fe98b39ca8ab025c42fda1c
Ryan Wright-Zinniger <ryr.zinn@gmail.com>
2023-03-08 14:38:24 -0800

    Readability and documentation updates

--- website/auth.py
+++ website/auth.py
@@ -11,139 +11,146 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
-        email_or_username = request.form.get("email-or-username")
-        password = request.form.get("password")
+    """Login all user types."""
 
-        # Logic for allowing users to enter username or email
-        from_email = User.query.filter_by(email=email_or_username).first()
-        from_username = User.query.filter_by(username=email_or_username).first()
-        user = from_email if from_email else from_username
+    # Edge Case - return to login page if method is not POST
+    if request.method != "POST":
+        return render_template("login.html", user=current_user)
 
-        if user:
-            if check_password_hash(user.password, password):
-                login_user(user, remember=True)
-                return redirect(url_for("views.home"))
-            else:
-                flash(f"Password entered for {email_or_username} is incorrect.", category="error")
+    # If the request method is POST, proceed with login
+    email_or_username = request.form.get("email-or-username")
+    password = request.form.get("password")
+
+    # Logic for allowing users to enter username or email
+    from_email = User.query.filter_by(email=email_or_username).first()
+    from_username = User.query.filter_by(username=email_or_username).first()
+    user = from_email if from_email else from_username
+
+    # If user is authenticated, log them in, else flash a warning
+    if user:
+        if check_password_hash(user.password, password):
+            login_user(user, remember=True)
+            return redirect(url_for("views.home"))
         else:
-            flash(f"There is no user account with email/username: {email_or_username}.", category="error")
+            flash(f"Password entered for {email_or_username} is incorrect.", category="error")
+    else:
+        flash(f"There is no user account with email/username: {email_or_username}.", category="error")
 
     return render_template("login.html", user=current_user)
 
 
 @auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
-    if request.method == "POST":
-        username = request.form.get("username")
-        email = request.form.get("email")
-        first_name = request.form.get("fName")
-        last_name = request.form.get("lName")
-        password1 = request.form.get("password1")
-        password2 = request.form.get("password2")
-        subscribe = True if (request.form.get("subscribe") == "subscribe") else False
+    """Sign-up page for a standard 'Viewer' account."""
 
-        email_exists = User.query.filter_by(email=email).first()
-        username_exists = User.query.filter_by(username=username).first()
+    # Edge Case - return to sign-up page if method is not POST
+    if request.method != "POST":
+        return render_template("signup.html", user=current_user)
 
-        if email_exists:
-            flash("Email is already in use!", category="error")
-        elif username_exists:
-            flash("Username is already in use!", category="error")
-        elif not first_name or not last_name:
-            flash("You must enter a First and Last name!")
-        elif password1 != password2:
-            flash("Passwords do not match!", category="error")
-        elif len(username) < 2:
-            flash("Username is too short!", category="error")
-        elif len(password1) < 6:
-            flash("Password is too short!", category="error")
-        else:
-            new_user = User(
-                email=email,
-                username=username,
-                first_name=first_name,
-                last_name=last_name,
-                password=generate_password_hash(password1, method="sha256"),
-                date_created=datetime.now(),
-                permissions="Viewer",
-                subscribed=subscribe,
-            )
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
-            return redirect(url_for("views.home"))
+    # If the request method is POST, proceed with sign-up
+    # Get all the data from the form
+    username = request.form.get("username")
+    email = request.form.get("email")
+    first_name = request.form.get("fName")
+    last_name = request.form.get("lName")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
+    # Logic for deciding if user wants to subscribe
+    subscribe = True if (request.form.get("subscribe") == "subscribe") else False
+
+    # Objects to check for edge cases
+    email_exists = User.query.filter_by(email=email).first()
+    username_exists = User.query.filter_by(username=username).first()
+
+    # Check for edge cases and if none, create the new user
+    if email_exists:
+        flash("Email is already in use!", category="error")
+    elif username_exists:
+        flash("Username is already in use!", category="error")
+    elif not first_name or not last_name:
+        flash("You must enter a First and Last name!")
+    elif password1 != password2:
+        flash("Passwords do not match!", category="error")
+    elif len(username) < 2:
+        flash("Username is too short!", category="error")
+    elif len(password1) < 6:
+        flash("Password is too short!", category="error")
+    else:
+        new_user = User(
+            email=email,
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=generate_password_hash(password1, method="sha256"),
+            date_created=datetime.now(),
+            permissions="Viewer",
+            subscribed=subscribe,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user, remember=True)
+        return redirect(url_for("views.home"))
 
     return render_template("signup.html", user=current_user)
 
 
 @auth.route("/sign-up-root", methods=["GET", "POST"])
 def sign_up_root():
-    if request.method == "POST":
-        username = request.form.get("username")
-        email = request.form.get("email")
-        first_name = request.form.get("fName")
-        last_name = request.form.get("lName")
-        password1 = request.form.get("password1")
-        password2 = request.form.get("password2")
+    """Sign-up page for a 'Root' account. Route hidden."""
 
-        email_exists = User.query.filter_by(email=email).first()
-        username_exists = User.query.filter_by(username=username).first()
+    # Edge Case - return to sign-up page if method is not POST
+    if request.method != "POST":
+        return render_template("signup.html", user=current_user)
 
-        if email_exists:
-            flash("Email is already in use!", category="error")
-        elif username_exists:
-            flash("Username is already in use!", category="error")
-        elif not first_name or not last_name:
-            flash("You must enter a First and Last name!")
-        elif password1 != password2:
-            flash("Passwords do not match!", category="error")
-        elif len(username) < 2:
-            flash("Username is too short!", category="error")
-        elif len(password1) < 6:
-            flash("Password is too short!", category="error")
-        else:
-            new_user = User(
-                email=email,
-                username=username,
-                first_name=first_name,
-                last_name=last_name,
-                password=generate_password_hash(password1, method="sha256"),
-                date_created=datetime.now(),
-                permissions="Root",
-                subscribed=False,
-            )
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
-            return redirect(url_for("views.home"))
+    # If the request method is POST, proceed with sign-up
+    # Get all the data from the form
+    username = request.form.get("username")
+    email = request.form.get("email")
+    first_name = request.form.get("fName")
+    last_name = request.form.get("lName")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
+
+    # Objects to check for edge cases
+    email_exists = User.query.filter_by(email=email).first()
+    username_exists = User.query.filter_by(username=username).first()
+
+    # Check for edge cases and if none, create the new user
+    if email_exists:
+        flash("Email is already in use!", category="error")
+    elif username_exists:
+        flash("Username is already in use!", category="error")
+    elif not first_name or not last_name:
+        flash("You must enter a First and Last name!")
+    elif password1 != password2:
+        flash("Passwords do not match!", category="error")
+    elif len(username) < 2:
+        flash("Username is too short!", category="error")
+    elif len(password1) < 6:
+        flash("Password is too short!", category="error")
+    else:
+        new_user = User(
+            email=email,
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=generate_password_hash(password1, method="sha256"),
+            date_created=datetime.now(),
+            permissions="Root",
+            subscribed=False,  # Root users need not subscribe
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user, remember=True)
+        return redirect(url_for("views.home"))
 
     return render_template("signup.html", user=current_user)
-
-
-@auth.route("/subscribe/<user_id>")
-@login_required
-def subscribe(user_id):
-    user = User.query.filter_by(id=user_id).first()
-
-    if user:
-        new_subscribption = False if user.subscribed else True
-        user.subscribed = new_subscribption
-        db.session.commit()
-
-        return jsonify(
-            {
-                "success": f"User's subscription status updated to: {new_subscribption}",
-                "subscribed": new_subscribption,
-                "status": 200,
-            }
-        )
-    else:
-        return jsonify({"error": "User's subscription status could not be updated", "status": 400})
 
 
 @auth.route("/logout")
 @login_required
 def logout():
+    """Log out a user. Redirect to home page."""
+
     logout_user()
     return redirect(url_for("views.home"))
--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
+from datetime import datetime
 from .models import Post, User, Comment, Like
 from . import db
-from datetime import datetime
 
 views = Blueprint("views", __name__)
 
@@ -10,6 +10,9 @@ views = Blueprint("views", __name__)
 @views.route("/")
 @views.route("/home")
 def home():
+    """Home view."""
+
+    # Define all posts and pass them to home template
     posts = Post.query.all()
 
     return render_template("home.html", user=current_user, posts=posts)
@@ -18,104 +21,122 @@ def home():
 @views.route("/create-post", methods=["GET", "POST"])
 @login_required
 def create_post():
-    if request.method == "POST":
-        # Define request parameters
-        title = request.form.get("title")
-        description = request.form.get("description") if request.form.get("description") else ""
-        text = request.form.get("text")
-        image = request.form.get("image") if request.form.get("image") else None
+    """Create a new post view."""
 
-        # Verify parameters
-        if not text or not title:
-            flash("Post cannot be empty!", category="error")
-            return render_template("create_post.html", user=current_user)
+    # Edge Case - return to create post page if method is not POST
+    if request.method != "POST":
+        return render_template("create_post.html", user=current_user)
 
-        # Create the new post if method is POST
-        post = Post(
-            title=title,
-            description=description,
-            text=text,
-            image=image,
-            author=current_user.id,
-            date_created=datetime.now(),
-            date_updated=datetime.now(),
-        )
-        db.session.add(post)
-        db.session.commit()
+    # Define request parameters
+    title = request.form.get("title")
+    description = request.form.get("description") if request.form.get("description") else ""
+    text = request.form.get("text")
+    image = request.form.get("image") if request.form.get("image") else None
 
-        return redirect(url_for("views.post", post_id=post.id))
+    # Edge Cases - Verify text and title parameters were passed
+    if not text or not title:
+        flash("Post cannot be empty!", category="error")
+        return render_template("create_post.html", user=current_user)
 
-    return render_template("create_post.html", user=current_user)
+    # Create the new post instance
+    post = Post(
+        title=title,
+        description=description,
+        text=text,
+        image=image,
+        author=current_user.id,
+        date_created=datetime.now(),
+        date_updated=datetime.now(),
+    )
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(url_for("views.post", post_id=post.id))
 
 
 @views.route("/edit-post/<post_id>")
 @login_required
 def edit_post(post_id):
-    """View for update post page."""
+    """Edit post view. Fills out the edit post form."""
+
+    # Get the instance of the post you will edit
     post = Post.query.filter_by(id=post_id).first()
 
+    # Edge Case - return home and warn user if the post does not exist
     if not post:
         flash("This post does not exist!", category="error")
         return render_template("home.html", user=current_user)
 
+    # Render the edit post page with all the post's current contents filled out
     return render_template("edit_post.html", user=current_user, post=post)
 
 
 @views.route("/update-post/<post_id>", methods=["GET", "POST"])
 @login_required
 def update_post(post_id):
-    """Update the post passed with the new info submitted"""
+    """Update the post passed with the new info submitted."""
 
+    # Edge Case - return home if the method is not post
+    if request.method != "POST":
+        return render_template(url_for("home.html"), user=current_user)
+
+    # Get the instance of the post you will update
     post = Post.query.filter_by(id=post_id).first()
 
+    # Edge Case - return home and warn user if the post does not exist
     if not post:
         flash("This post does not exist!", category="error")
         return render_template("home.html", user=current_user)
 
-    # Declare updated info
+    # Get updated info from form
     title = request.form.get("title")
     description = request.form.get("description")
     text = request.form.get("text")
     image = request.form.get("image")
 
+    # Edge Cases - Ensure you have the info you need
     if not (title or text or image):
         flash("All fields are required except description!", category="error")
         return render_template("home.html", user=current_user)
 
     # Update the post object
-    if request.method == "POST":
-        post.title = title
-        post.description = description
-        post.text = text
-        post.image = image
-        post.date_updated = datetime.now()
-        db.session.commit()
+    post.title = title
+    post.description = description
+    post.text = text
+    post.image = image
+    post.date_updated = datetime.now()
+    db.session.commit()
 
-        return redirect(url_for("views.post", post_id=post.id))
-
-    return render_template(url_for("home.html"), user=current_user)
+    return redirect(url_for("views.post", post_id=post.id))
 
 
 @views.route("/delete-post/<id>")
 @login_required
 def delete_post(id):
+    """Delete a post."""
+
+    # Get the instance of the post you will delete
     post = Post.query.filter_by(id=id).first()
 
+    # Edge Cases - Ensure post exists & user can delete it
     if not post:
         return jsonify({"error": "Post does not exist.", "status": 400})
-    elif current_user.id == post.id:
+    if current_user.id == post.id:
         return jsonify({"error": "You do not have permission to delte this post.", "status": 401})
-    else:
-        db.session.delete(post)
-        db.session.commit()
+
+    # Delete post instance
+    db.session.delete(post)
+    db.session.commit()
 
     return jsonify({"success": "Post deleted successfully.", "status": 200})
 
 
 @views.route("/posts")
 def all_posts():
-    """View for all posts."""
-    posts = Post.query.filter_by()
+    """All posts view."""
+
+    # Get all the posts and pass them to the posts page
+    posts = Post.query.all()
 
     return render_template("posts.html", user=current_user, posts=posts)
 
@@ -123,19 +144,27 @@ def all_posts():
 @views.route("/posts/<username>")
 @login_required
 def posts(username):
-    """View for all posts for a given user."""
+    """View for all posts for a given user. Currently hidden."""
+
+    # Get intance of user
     user = User.query.filter_by(username=username).first()
+
+    # Edge Case - redirect to home page if user does not exist
     if not user:
         flash("No user with that username exists!", category="error")
         return redirect(url_for("views.home"))
 
+    # Get user's posts and pass them to the page
     posts = user.posts
+
     return render_template("users_posts.html", user=current_user, posts=posts, username=username)
 
 
 @views.route("/post/<post_id>")
 def post(post_id):
-    """View for a single post"""
+    """Single post view."""
+
+    # Get instance of the post you want to display andpass it to post page
     post = Post.query.filter_by(id=post_id).first()
 
     return render_template("post.html", user=current_user, post=post)
@@ -144,47 +173,55 @@ def post(post_id):
 @views.route("/create-comment/<post_id>", methods=["POST"])
 @login_required
 def create_comment(post_id):
-    """Creates a comment if the request method is POST"""
+    """Create a comment."""
+
+    # Edge Case - If method is not post, redirect to all posts page
+    if request.method != "POST":
+        return redirect(url_for("views.all_posts"))
 
     # Get the comment and the post the comment corresponds to
     comment = request.form.get("text")
     post = Post.query.filter_by(id=post_id).first()
 
-    # Check for edge cases - empty comment & post doesn't exist
+    # Check for edge cases - empty comment & nonexistant post
     if not comment:
         flash("Comment cannot be empty!", category="error")
         return render_template("post.html", user=current_user, post=post)
     if not post:
         return jsonify({"error": "Post does not exist!", "status": 400})
 
-    # If the request method is POST, write the comment to the post and refresh the page
-    if request.method == "POST":
-        comment = Comment(text=comment, author=current_user.id, date_created=datetime.now(), post_id=post_id)
-        db.session.add(comment)
-        db.session.commit()
-        return render_template("post.html", user=current_user, post=post)
-
-    return redirect(url_for("views.all_posts"))
+    # Write the comment to the post and refresh the page
+    comment = Comment(text=comment, author=current_user.id, date_created=datetime.now(), post_id=post_id)
+    db.session.add(comment)
+    db.session.commit()
+    return render_template("post.html", user=current_user, post=post)
 
 
 @views.route("/delete-comment/<comment_id>")
 @login_required
 def delete_comment(comment_id):
+    """Delete Comment."""
+
+    # Get instance of comment to be deleted
     comment = Comment.query.filter_by(id=comment_id).first()
 
+    # Edge Cases - ensure comment exists and user has correct permissions
     if not comment:
         return jsonify({"error": "Comment does not exist.", "status": 400})
-    elif current_user.id != comment.author and current_user.id != comment.post.author:
+    if current_user.id != comment.author and current_user.id != comment.post.author:
         return jsonify({"error": "You do not have permission to delete this comment.", "status": 400})
-    else:
-        db.session.delete(comment)
-        db.session.commit()
+
+    # Delete the comment
+    db.session.delete(comment)
+    db.session.commit()
 
     return jsonify({"success": "Comment deleted.", "status": 200})
 
 
 @views.route("/like-post/<post_id>", methods=["POST"])
 def like(post_id):
+    """Like a Post: Create or delete like object."""
+
     # Initially check that user is logged in. Return error if not.
     if not current_user.is_authenticated:
         flash("You must log in to like posts!", category="error")
@@ -194,18 +231,17 @@ def like(post_id):
     post = Post.query.filter_by(id=post_id).first()
     like = Like.query.filter_by(author=current_user.id, post_id=post_id).first()
 
-    # Check that the posts exists. Return error if not.
+    # Edge Case - check that the posts exists. Return error if not
     if not post:
         return jsonify({"error": "Post does not exist.", "status": 400})
 
-    # Update the like count.
+    # Logic to update the like instance
     if like:
         db.session.delete(like)
-        db.session.commit()
     else:
         like = Like(author=current_user.id, date_created=datetime.now(), post_id=post_id)
         db.session.add(like)
-        db.session.commit()
+    db.session.commit()
 
     # Return the amount of likes and bool of whether user has like the post or not.
     return jsonify(
@@ -214,23 +250,47 @@ def like(post_id):
 
 
 @views.route("/pin-post/<post_id>", methods=["POST"])
+@login_required
 def pin(post_id):
-    # Retreive the post and like objects given the post id and current user's id
-    post = Post.query.filter_by(id=post_id).first()
-    pinned = post.pinned
+    """Pin a Post: Toggle it's pinned status"""
 
-    # Check that the posts exists. Return error if not.
+    # Retreive the post and pinned instances
+    post = Post.query.filter_by(id=post_id).first()
+    currently_pinned = post.pinned
+
+    # Edge Case - check that the post exists
     if not post:
         return jsonify({"error": "Post does not exist.", "status": 400})
 
-    # Update the pin status.
-    if pinned:
-        pinned = False
-        post.pinned = pinned
-    else:
-        pinned = True
-        post.pinned = pinned
+    # Logic to update the pinned status
+    post.pinned = False if currently_pinned else True
     db.session.commit()
 
     # Return the amount of likes and bool of whether user has like the post or not.
-    return jsonify({"pinned": pinned, "status": 200})
+    return jsonify({"pinned": post.pinned, "status": 200})
+
+
+@views.route("/subscribe/<user_id>")
+@login_required
+def subscribe(user_id):
+    """Toggles a user's subscription status between True and False."""
+
+    # Get the user instance you will be updating
+    user = User.query.filter_by(id=user_id).first()
+
+    # Check the edge case of a user not existing
+    if not user:
+        return jsonify({"error": "User's subscription status could not be updated", "status": 400})
+
+    # Logic to toggle user's subscription status
+    new_subscription = False if user.subscribed else True
+    user.subscribed = new_subscription
+    db.session.commit()
+
+    return jsonify(
+        {
+            "success": f"User's subscription status updated to: {new_subscription}",
+            "subscribed": new_subscription,
+            "status": 200,
+        }
+    )
