e3452cb67b7e162eb95d954a2355b497a82c7832
Ryan Wright-Zinniger <ryr.zinn@gmail.com>
2023-03-08 15:01:46 -0800

    Readability and documentation updates

--- website/static/index.js
+++ website/static/index.js
@@ -1,52 +1,63 @@
 function like(postId) {
-    const likeCount = document.getElementById(`likes-count-${postId}`);
-    const likeButton = document.getElementById(`like-button-${postId}`);
+    // Invoke like-post function & update like count/icon in frontend dynamically.
   
     fetch(`/like-post/${postId}`, { method: "POST" })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data["status"] === 200) {
-            likeCount.innerHTML = data["likes"];
-            if (data["liked"] === true) {
-                likeButton.className = "fas fa-thumbs-up";
-            } else {
-                likeButton.className = "far fa-thumbs-up";
-            }
+    .then((res) => res.json())
+    .then((data) => {
+    // If call successful...
+    if (data["status"] === 200) {
+        // Get like objects to be updated
+        const likeButton = document.getElementById(`like-button-${postId}`);
+        const likeCount = document.getElementById(`likes-count-${postId}`);
+        // Update like count in html
+        likeCount.innerHTML = data["likes"];
+        // Toggle the like button's image
+        if (data["liked"] === true) {
+            likeButton.className = "fas fa-thumbs-up";
+        } else {
+            likeButton.className = "far fa-thumbs-up";
         }
-        if (data["status"] === 401) {
-            window.location.replace("/login");
-        }
-      })
-      .catch((e) => alert("Could not like post."));
+    }
+    // If unsuccessful because of permissions, redirect to login page
+    if (data["status"] === 401) {
+        window.location.replace("/login");
+    }
+    })
+    // Catch any unforseen errors and throw generic alert   
+    .catch((e) => alert("Could not like post."));
   }
 
 
 function delete_comment(commentId) {
-    const comment = document.getElementById(`comment-${commentId}`)
+    // Invoke delete comment function & remove comment from frontend dynamically.
   
     fetch(`/delete-comment/${commentId}`, {}) 
     .then((res) => res.json())
     .then((data) => {
+        // If invocation successful, delete the comment from page
         if (data["success"]) {
+            const comment = document.getElementById(`comment-${commentId}`)
             comment.remove();
         }
     })
+    // Catch any unforseen errors and throw generic alert   
     .catch((e) => alert("Could not delete comment."));
   }
 
 
 function delete_post(postId) {
-    // Confirm user want's to delete post. Return if not.
+    // Invoke delete post view & remove post's card from frontend dynamically. 
+    
+    // Confirm user want's to delete post. Return if not
     const response = confirm("Are you sure you want to do delete this post?");
     if (response === false) {
         return
     }
 
-    // Call to delete post
     fetch(`/delete-post/${postId}`, {}) 
     .then((res) => res.json())
     .then((data) => {
-    // Handle call response
+        // If post is successfully deleted
         if (data["status"] == 200) {
             // Define post card element
             const postCard = document.getElementById(`post-card-${postId}`)
@@ -60,55 +71,66 @@ function delete_post(postId) {
             }
         }
     })
+    // Catch any unforseen errors and throw generic alert   
     .catch((e) => alert("Could not delete post."));
 }
 
 
 function pin(postId) {
-    const pinButton = document.getElementById(`pin-button-${postId}`);
+    // Invoke pin function & update pin icon dynamically.
   
     fetch(`/pin-post/${postId}`, { method: "POST" })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data["status"] === 200) {
-            // pinned.innerHTML = data["pinned"];
-            if (data["pinned"] === true) {
-                pinButton.className = "param fa-solid fa-check";
-            } else {
-                pinButton.className = "param fa-solid fa-location-pin";
-            }
+    .then((res) => res.json())
+    .then((data) => {
+    // If pin updated successfully, update icon it in html
+    if (data["status"] === 200) {
+        const pinButton = document.getElementById(`pin-button-${postId}`);
+        if (data["pinned"] === true) {
+            pinButton.className = "param fa-solid fa-check";
+        } else {
+            pinButton.className = "param fa-solid fa-location-pin";
         }
-      })
-      .catch((e) => alert("Could not pin post."));
+    }
+    })
+    // Catch any unforseen errors and throw generic alert  
+    .catch((e) => alert("Could not pin post."));
   }
 
 
 function toggle_subscription(userId) {
-    // When the mail icon in the navbar is clicked on, this toggles the user's subscription status
-    // and updates the icon from a hollow envelope to a solid one.
-    // const subscriptionStatus = document.getElementById(`subscription-status-${userId}`)
-    const subscribedIcon = document.getElementById(`subscribed-${userId}`);
-    const subscribedHomeIcon = document.getElementById(`subscribed-home-${userId}`)
-    const subscribedHomeText = document.getElementById(`subscribed-text-${userId}`)
+    // Invoke subscribe function & update subscribed icon(s) dynamically.
     
     fetch(`/subscribe/${userId}`)
-        .then((res) => res.json())
-        .then((data) => {
-            if (data["status"] === 200) {
-                // subscriptionStatus.innerHTML = data["subscribed"];
-                if (data["subscribed"] === true) {
-                    subscribedIcon.className = "nav-item nav-link fa-solid fa-envelope";
+    .then((res) => res.json())
+    .then((data) => {
+        // If subscription toggled successfully, update the icons 
+        if (data["status"] === 200) {
+            const subscribedNavIcon = document.getElementById(`subscribed-${userId}`);
+            const subscribedHomeIcon = document.getElementById(`subscribed-home-${userId}`)
+            const subscribedHomeText = document.getElementById(`subscribed-text-${userId}`)
+            if (data["subscribed"] === true) {
+                // Always update nav icon
+                subscribedNavIcon.className = "nav-item nav-link fa-solid fa-envelope";
+                // Only update Home page icon if on home page
+                if (subscribedHomeIcon) {
                     subscribedHomeIcon.className = "nav-item nav-link fa-solid fa-envelope fa-5x";
                     subscribedHomeText.innerHTML = "You are currently subscribed!<br>To unsubscribe, clicke the icon below..."
-                } else {
-                    subscribedIcon.className = "nav-item nav-link fa-regular fa-envelope";
+                }
+            } else {
+                // Always update nav icon
+                subscribedNavIcon.className = "nav-item nav-link fa-regular fa-envelope";
+                // Only update Home page icon if on home page
+                if (subscribedHomeIcon) {
                     subscribedHomeIcon.className = "nav-item nav-link fa-regular fa-envelope fa-5x";
                     subscribedHomeText.innerHTML = "You are currently not subscribed!<br>Click the icon below to subscribe..."
                 }
             }
-            if (data["status"] === 400) {
-                window.location.replace("/login");
-            }
-      })
-      .catch((e) => alert(e));
+        }
+        // If failed, return to login page
+        if (data["status"] === 400) {
+            window.location.replace("/login");
+        }
+    })
+    // Catch any unforseen errors and throw generic alert   
+    .catch((e) => alert("Could not toggle subscription status."));
 }
